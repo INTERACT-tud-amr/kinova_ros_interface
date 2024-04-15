7d45f01364513522bcc1dc584beff8347e979807
TomasMerva <tmerva7@gmail.com>
2024-04-15 10:33:52 +0200

    ros format v0

---
+++ scripts/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import rospy
+
+if __name__ == "__main__":
+    # Ros initialization
+    rospy.init_node("kinova_driver")
+    
---
+++ src/kinova_ros_interface/kinova_client.py
@@ -0,0 +1,7 @@
+from kortex_api.RouterClient import RouterClient
+from kortex_api.autogen.client_stubs.BaseClientRpc import BaseClient
+from kortex_api.autogen.client_stubs.BaseCyclicClientRpc import BaseCyclicClient
+from kortex_api.autogen.client_stubs.ActuatorConfigClientRpc import ActuatorConfigClient
+from kortex_api.autogen.messages import Base_pb2, BaseCyclic_pb2, ActuatorConfig_pb2
+
+
